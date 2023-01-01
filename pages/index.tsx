b34c38084d80c00d50892d8259261c6d5320bb9f
Sunrit Jana <warriordefenderz@gmail.com>
2023-01-01 18:14:38 +0530

    start implementing the lessons page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,38 @@
-const Home = () => {
+import Link from 'next/link';
+
+import { supabase } from '../lib/supabase';
+
+import type { Lesson } from '../types';
+
+interface Props {
+  lessons: Lesson[];
+}
+
+const Home = ({ lessons }: Props) => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      Hello, World!
+    <div className="w-full max-w-3xl mx-auto my-8 px-2">
+      {lessons.map(lesson => (
+        <Link href={`/lesson/${lesson.id}`} key={lesson.id}>
+          <div className="block bg-white shadow-md rounded-lg overflow-hidden my-4">
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">{lesson.title}</div>
+              <p className="text-gray-700 text-base">{lesson.description}</p>
+            </div>
+          </div>
+        </Link>
+      ))}
     </div>
   );
+};
+
+export const getServerSideProps = async () => {
+  const { data: lessons } = await supabase.from('lesson').select('*');
+
+  return {
+    props: {
+      lessons
+    }
+  };
 };
 
 export default Home;
---
+++ types.ts
@@ -0,0 +1,6 @@
+export interface Lesson {
+  id: number;
+  title: string;
+  description: string;
+  created_at: string;
+}
