71b09eb93d667151faaed57649c336bb10d6ee3e
Sunrit Jana <warriordefenderz@gmail.com>
2023-01-11 22:19:43 +0530

    Navbar done

---
+++ components/Navbar.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link"
+
+import { useUser } from "../context/user"
+
+
+const Navbar = () => {
+    const {user} = useUser()
+
+  return (
+    <div className="flex py-4 px-8 border-b border-gray-200">
+        <Link href="/">
+            Home
+        </Link>
+        <Link href="/pricing" className="ml-4">
+            Pricing
+        </Link>
+
+        <Link href={user ? "/logout" : "/login"} className="ml-auto">
+            {user ? "Logout" : "Login"}
+        </Link>
+    </div>
+  )
+}
+
+export default Navbar
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import '../styles/globals.css';
 
+import Navbar from '../components/Navbar';
+
 import type { AppProps } from 'next/app';
 import { UserProvider } from '../context/user';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
+      <Navbar />
       <Component {...pageProps} />
     </UserProvider>
   );
