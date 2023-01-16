c0d1c01c14b23884d7d91a659942f5bec34617c5
Sunrit Jana <warriordefenderz@gmail.com>
2023-01-16 21:29:49 +0530

    Update README, merge SQL files, update .env.example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 
 import { supabase } from '../lib/supabase';
-import { useUser } from '../context/user';
 
 import type { Lesson } from '../types';
 
--- sql/schema.sql
+++ sql/schema.sql
@@ -15,20 +15,22 @@ CREATE TABLE profile (
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     is_subscribed BOOLEAN NOT NULL DEFAULT FALSE,
-    interval VARCHAR(255) NOT NULL,  -- monthly, yearly, etc.
+    interval VARCHAR(255),  -- monthly, yearly, etc. empty for none.
 
-    stripe_customer VARCHAR(255),
+    stripe_customer VARCHAR(255),  -- set later using realtime INSERT trigger on signup
 
     CONSTRAINT fk_profile_user FOREIGN KEY (id) REFERENCES auth.users (id)
 );
 
 CREATE TABLE premium_content (
-    -- ID foreign key to the lesson table
-    id INTEGER REFERENCES lesson (id),
+    id SERIAL PRIMARY KEY,
 
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     video_url VARCHAR(255) NOT NULL,
+
+    -- ID foreign key to the lesson table
+    CONSTRAINT fk_premium_content_lesson FOREIGN KEY (id) REFERENCES lesson (id)
 )
 
 -- Define a function - `create_profile_for_user`
@@ -45,3 +47,26 @@ $$ LANGUAGE plpgsql;
 -- It will call the `create_profile_for_user` function
 CREATE TRIGGER create_profile_for_user AFTER INSERT ON auth.users
     FOR EACH ROW EXECUTE FUNCTION create_profile_for_user();
+
+-- Enable RLS for public.lesson and public.profile
+ALTER TABLE public.lesson ENABLE ROW LEVEL SECURITY;
+ALTER TABLE public.profile ENABLE ROW LEVEL SECURITY;
+ALTER TABLE public.premium_content ENABLE ROW LEVEL SECURITY;
+
+-- The policies for row-level security in supabase
+CREATE POLICY "select_lesson_for_all" ON "public"."lesson"
+AS PERMISSIVE FOR SELECT
+TO PUBLIC
+USING (true);
+
+CREATE POLICY "select_profile_for_respective_user" ON "public"."profile"
+AS PERMISSIVE FOR SELECT
+TO authenticated
+USING (auth.uid() = id);
+
+CREATE POLICY "premium_content_for_subscribed_users" ON "public"."premium_content"
+AS PERMISSIVE FOR SELECT
+TO public
+USING (
+    exists( SELECT 1 FROM profile WHERE auth.uid() = profile.id AND profile.is_subscribed = true )
+)
