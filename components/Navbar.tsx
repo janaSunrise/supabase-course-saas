e593eb2bf6b63d8baba8c1b2e7427244d1916183
Sunrit Jana <warriordefenderz@gmail.com>
2023-01-16 12:16:10 +0530

    Create the basic dashboard with simple details display

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,6 +8,13 @@ const Navbar = () => {
   return (
     <div className="flex py-4 px-8 border-b border-gray-200">
       <Link href="/">Home</Link>
+
+      {!!user && (
+        <Link href="/dashboard" className="ml-4">
+          Dashboard
+        </Link>
+      )}
+
       <Link href="/pricing" className="ml-4">
         Pricing
       </Link>
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,5 +1,49 @@
+import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs';
+
+import { useUser } from '../context/user';
+
+import type { GetServerSidePropsContext } from 'next';
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const { user, isLoading } = useUser();
+
+  return (
+    <div className="w-full max-w-3xl mx-auto py-16 px-8">
+      <h1 className="text-4xl font-bold mb-6">Dashboard</h1>
+      <p className="text-gray-700 text-lg mb-4">
+        Welcome back, {user?.profile?.email}!
+      </p>
+      {!isLoading && (
+        <p className="text-gray-700 text-lg mb-4">
+          {user?.profile?.is_subscribed
+            ? `Subscribed: ${user?.profile?.interval}`
+            : 'Not subscribed'}
+        </p>
+      )}
+    </div>
+  );
+};
+
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  const supabase = createServerSupabaseClient(ctx);
+
+  const {
+    data: { user }
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false
+      },
+      props: {}
+    };
+  }
+
+  return {
+    props: {}
+  };
 };
 
 export default Dashboard;
